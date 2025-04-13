be15bbba3ac8b06d4abdd773e51de32d353a03c4
jamogha <jamoghap@gmail.com>
2025-04-13 01:49:59 +0100

    positive login

---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test.only('test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.getByText('You logged into a secure area')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Secure Area', exact: true })).toBeVisible();
+});
